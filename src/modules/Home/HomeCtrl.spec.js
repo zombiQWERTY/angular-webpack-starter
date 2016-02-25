7baf0ee21b39fd31371908db47b5f17e21cf25d0
zombiQWERTY <zombiqwerty@yandex.ru>
2016-02-26 01:01:42 +0300

    Renamed specs

---
+++ src/modules/Home/HomeCtrl.spec.js
@@ -0,0 +1,17 @@
+import Home from './index';
+
+describe('Controller: Home', () => {
+  //beforeEach(module(Layout));
+  //
+  //var $controller;
+  //
+  //beforeEach(inject(_$controller_ => $controller = _$controller_));
+  //
+  //describe('Is Layout defined?', () => {
+  //  it('Is Layout defined?', () => {
+  //    var $scope = {};
+  //    var controller = $controller('HomeController', { $scope });
+  //    expect(controller).toBeDefined();
+  //  });
+  //});
+});
---
+++ src/modules/Layout/LayoutCtrl.spec.js
@@ -0,0 +1,17 @@
+import Layout from './index';
+
+describe('Controller: Layout', () => {
+  //beforeEach(module(Layout));
+  //
+  //var $controller;
+  //
+  //beforeEach(inject(_$controller_ => $controller = _$controller_));
+  //
+  //describe('Is Layout defined?', () => {
+  //  it('Is Layout defined?', () => {
+  //    var $scope = {};
+  //    var controller = $controller('LayoutController', { $scope });
+  //    expect(controller).toBeDefined();
+  //  });
+  //});
+});
